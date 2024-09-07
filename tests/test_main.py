cb1fac32ae813bc97385a980310e9fd5347d6991
bugbusterqa <bugbusterqa@rambler.ru>
2024-09-07 12:29:49 +0300

    fixtures

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,14 @@
 """
 2024 (c) QA Automation
 """
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 
-def test_smoke():
+def test_smoke(browser):
     """
     SMK-1. Smoke test
     """
-    chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("start-maximized")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("--disable-extensions")
+    browser.get("https://demoqa.com/text-box")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    driver.get("https://demoqa.com/text-box")
-
-    button = driver.find_element(By.ID, "submit")
-
+    button = browser.find_element(By.ID, "submit")
     assert button.text == "Submit", "Button text is incorrect"
